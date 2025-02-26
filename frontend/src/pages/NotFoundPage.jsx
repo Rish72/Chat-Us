22d4f3d0293ddf731d2fe26ccd54f2fe8fa40282
Rishabh <rishabhgoel2020@gmail.com>
2025-02-26 13:52:27 +0530

    Update CORS policy in backend, Complete login and signup pages in frontend

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -2,7 +2,7 @@
 
 function NotFoundPage() {
   return (
-    <div><h1 className="text-center text-3xl text-red-500">
+    <div className="flex items-baseline justify-baseline"><h1 className="text-center text-3xl text-red-500">
         NOT FOUND 404 </h1>
         </div>
   )
