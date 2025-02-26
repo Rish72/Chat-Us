27e1aa86b9842d186e1334851171a9bebd8129c3
Rishabh <rishabhgoel2020@gmail.com>
2025-02-26 23:54:38 +0530

    Add Cors policy

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,19 @@ import dotenv from 'dotenv';
 import authRouter from "./routes/auth.routes.js"
 import msgRouter from "./routes/msg.routes.js"
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { connectDb } from './lib/db.js';
 
 dotenv.config()
+
 const app = express();
 app.use(cookieParser());        // cookie parser
+app.use(cors({
+    origin: "http://localhost:5173",  
+    credentials: true,  
+    methods: ["GET", "POST", "PUT", "DELETE"],  //  Correctly formatted HTTP methods
+}));
+
 app.use(express.json());        // body parser
 
 app.use("/api/auth", authRouter);
